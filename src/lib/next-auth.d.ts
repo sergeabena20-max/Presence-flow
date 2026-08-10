2b03200b2ddb51eac50988d154326b7051761fca
sergeabena20-max <sergeabena20@gmail.com>
2026-08-10 12:01:12 +0100

    Update next-auth.d.ts

--- src/lib/next-auth.d.ts
+++ src/lib/next-auth.d.ts
@@ -1,1 +1,34 @@
+import { UserRole } from "@prisma/client";
+import "next-auth";
+import "next-auth/jwt";
 
+declare module "next-auth" {
+  interface User {
+    id: string;
+    firstName: string;
+    lastName: string;
+    role: UserRole;
+    isActive: boolean;
+  }
+
+  interface Session {
+    user: {
+      id: string;
+      email: string;
+      firstName: string;
+      lastName: string;
+      role: UserRole;
+      isActive: boolean;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    firstName: string;
+    lastName: string;
+    role: UserRole;
+    isActive: boolean;
+  }
+}
