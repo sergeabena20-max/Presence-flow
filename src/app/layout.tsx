39f0b8c5408cff8213a23ede54ac6b2e759bcfab
sergeabena20-max <sergeabena20@gmail.com>
2026-08-17 13:54:33 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../globals.css";
 import { AuthSessionProvider } from "@/components/providers/session-provider";
 
 export const metadata: Metadata = {
