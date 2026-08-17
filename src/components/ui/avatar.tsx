7c29f7c15023f9c441b398ca3aec702339eb9ef8
sergeabena20-max <sergeabena20@gmail.com>
2026-08-17 17:49:47 +0100

    Update avatar.tsx

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -5,7 +5,7 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar";
 import { cn } from "@/lib/utils";
 
 const Avatar = React.forwardRef
-  React.ComponentRef<typeof AvatarPrimitive.Root>,
+  HTMLSpanElement,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Root
@@ -20,7 +20,7 @@ const Avatar = React.forwardRef
 Avatar.displayName = AvatarPrimitive.Root.displayName;
 
 const AvatarImage = React.forwardRef
-  React.ComponentRef<typeof AvatarPrimitive.Image>,
+  HTMLImageElement,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
@@ -32,7 +32,7 @@ const AvatarImage = React.forwardRef
 AvatarImage.displayName = AvatarPrimitive.Image.displayName;
 
 const AvatarFallback = React.forwardRef
-  React.ComponentRef<typeof AvatarPrimitive.Fallback>,
+  HTMLSpanElement,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
